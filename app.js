c5f9e4248b6a0538fa2011ca206318554471346f
ScottSyverson <166549266+ScottSyverson@users.noreply.github.com>
2024-09-06 08:17:35 -0500

    date entry working
    date entry working

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let dob = document.getElementById('dateOfBirth');
+
+//console.log(dob.value);
+
+let button = document.getElementById("submitButton")
+
+button.addEventListener("click", verifyAge)
+
+function verifyAge(){
+    console.log(dob.value);
+}
+
+
+
+
+
