f44b921c0380241ce28eb3fed0158366ad771619
ScottSyverson <166549266+ScottSyverson@users.noreply.github.com>
2024-09-06 14:15:33 -0500

    functional and looking ok

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 
 let dob = document.getElementById('dateOfBirth');
 
-//console.log(dob.value);
-
 let button = document.getElementById("submitButton")
 
 button.addEventListener("click", verifyAge)
 
 function verifyAge(){
     console.log(dob.value);
+    let birthday = dob.value;
+    let [bYear, bMonth, bDay] = birthday.split('-');
+    console.log(bYear);
+    let today = new Date().toISOString().slice(0, 10);
+    let [tYear, tMonth, tDay] = today.split('-');
+
+    console.log(tYear);
+    console.log(today)
+
+    let diffYear = tYear-bYear;
+    let verification = document.querySelector(".verified")
+    let invite = document.querySelector(".entrance")
+
+    if (diffYear < 18 && bMonth < tMonth && bDay < tDay ){
+        verification.style.color = "red"
+        verification.innerHTML ="You are not 18 yet!";
+    } else{
+        verification.style.color = "navy"
+        verification.innerHTML ="You are a grown ass adult and can do whatever you want!";
+    }
+    
 }
-
-
-
 
 
