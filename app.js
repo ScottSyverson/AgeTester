5f2965c33599435d788a749a4d69bba9c6185816
ScottSyverson <166549266+ScottSyverson@users.noreply.github.com>
2024-09-09 10:55:37 -0500

    clean up the code
    delete empty lines, line up elements, etc

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let button = document.getElementById("submitButton")
 
 button.addEventListener("click", verifyAge)
 
-function verifyAge(){
+function verifyAge() {
     console.log(dob.value);
     let birthday = dob.value;
     let [bYear, bMonth, bDay] = birthday.split('-');
@@ -16,18 +16,17 @@ function verifyAge(){
     console.log(tYear);
     console.log(today)
 
-    let diffYear = tYear-bYear;
+    let diffYear = tYear - bYear;
     let verification = document.querySelector(".verified")
     let invite = document.querySelector(".entrance")
 
-    if (diffYear < 18 && bMonth < tMonth && bDay < tDay ){
+    if (diffYear < 18 && bMonth < tMonth && bDay < tDay) {
         verification.style.color = "red"
-        verification.innerHTML ="You are not 18 yet!";
-    } else{
+        verification.innerHTML = "You are not 18 yet!";
+    } else {
         verification.style.color = "navy"
-        verification.innerHTML ="You are a grown ass adult and can do whatever you want!";
+        verification.innerHTML = "You are a grown ass adult and can do whatever you want!";
     }
-    
 }
 
 
